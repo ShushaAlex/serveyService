69fede0e9a8ab0fec44724f54b483d2ecb31d590
Alex <alexandra.grigorchenko@gmail.com>
2024-03-08 14:26:46 +0100

    add docs

--- src/main/java/com/example/spring/example/task2/component/service/converter/ConverterToQuestion.java
+++ src/main/java/com/example/spring/example/task2/component/service/converter/ConverterToQuestion.java
@@ -4,5 +4,10 @@ import com.example.spring.example.task2.model.entity.Question;
 
 public interface ConverterToQuestion {
 
+    /**
+     * convert string into Question
+     * @param string
+     * @return Question
+     */
     Question stringToQuestion(String string);
 }
